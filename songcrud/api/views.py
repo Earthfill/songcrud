253e873ef8c8892cc558756a21da4a1e86a99225
Earthfill <kingsley.omiy@gmail.com>
2022-11-04 18:39:05 +0100

    follow up

--- songcrud/api/views.py
+++ songcrud/api/views.py
@@ -1,66 +1,64 @@
 from django.http import JsonResponse
 from .serializers import ArtisteSerializer, SongSerializer
 from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
 from musicapp.models import Artiste, Song
 
 
 # Create your views here.
-@csrf_exempt
+@api_view(["GET", "POST"])
 def artiste_list_api(request):
     if request.method == "GET":
         artistes = Artiste.objects.all()
         serializer = ArtisteSerializer(artistes, many=True)
         print(serializer.data)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data, status=HTTP_200_OK)
     if request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = ArtisteSerializer(data=data)
+        serializer = ArtisteSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, safe=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, safe=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(["GET", "POST"])
 def song_list_api(request):
     if request.method == "GET":
         songs = Song.objects.all()
         serializer = SongSerializer(songs, many=True)
         print(serializer.data)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data, safe=False)
     if request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = SongSerializer(data=data)
+        serializer = SongSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, safe=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, safe=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(["GET", "POST", "PUT", "DELETE"])
 def song_detail_api(request, api):
     try:
         song = Song.objects.get(id=id)
     except Song.DoesNotExist:
-        return JsonResponse({"message":"Song not found"}, status=404)
+        return Response({"message":"Song not found"}, status=HTTP_404_NOT_FOUND)
     if request.method == "GET":
         serializer = SongSerializer(song)
         print(serializer.data)
-        return JsonResponse(serializer.data, safe=200)
+        return Response(serializer.data, safe=HTTP_200_OK)
     if request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = SongSerializer(data=data)
+        serializer = SongSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, safe=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, safe=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
     if request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = SongSerializer(Song, data=data)
+        serializer = SongSerializer(Song, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, safe=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, safe=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
     if request.method == "DELETE":
         song.delete()
-        return JsonResponse({"message":"Song deleted"}, status=204)
+        return Response({"message":"Song deleted"}, status=HTTP_204_NO_CONTENT)
 
